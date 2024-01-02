f90689339507c7a8ff77147ecbfa178401568c1b
aomsumita <sumita_n@ac.th.>
2024-01-02 11:26:55 +0700

    lab12 completed

---
+++ 12_5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <ctime>
+using namespace std;
+int fibonacci(long long int  x) ;
+
+int main(){
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+int fibonacci(long long int  x){
+    if (x>1){
+        return fibonacci(x-1) + fibonacci(x-2) ;
+    }else 
+    return x;
+}
